80a06c75245d6eb529ac8af12a7deb03f88b0f77
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-13 15:29:48 -0400

    tile click modal / close button

--- src/dom.js
+++ src/dom.js
@@ -3,20 +3,24 @@ const $ = function(ele) {
 };
 
 const showProperty = (ele) => {
-    console.log(ele);
     let propModal = $('#property-modal');
     let propModalContent = $('#prop-modal-content');
     let cardName = ele.split('-')[1];
+    let close = $('#close-prop');
 
+    //show modal
     propModal.style.display = "flex";
     propModalContent.style.backgroundImage = `url("./assets/Cards/${cardName}.jpg")`;
 
     //Close if click outside
-    window.onclick = function(e) {
-        if (e.target == propModal) {
+    window.onclick = e => {
+        if (e.target === propModal) {
             propModal.style.display = "none";
         }
-    }
+        if (e.target === close) {
+            propModal.style.display = "none";
+        }
+    };
 };
 
 document.querySelectorAll('.grid').forEach((e, i) => {
