cf0de8306f25c7a40849533e50d791249fe530f0
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-13 23:08:51 -0400

    import dom.js to main after merge

--- src/dom.js
+++ src/dom.js
@@ -2,186 +2,190 @@ const $ = function(ele) {
     return document.querySelector(ele);
 };
 
+const dom = () => {
+    //////////////////////Start Page-load Modal//////////////////////
+    let startModal = $('#start-modal');
+    let bodyWrap = $('#body-wrap');
+    let pSelect = document.querySelectorAll('.p-select-btn');
+    let pSelected = 3;
+    let p2 = $('#p2-name');
+    let p3 = $('#p3-name');
+    let p4 = $('#p4-name');
+    let modalClose = $('#modalClose');
+    let p1Target = $('#p1Target');
+    let p2Target = $('#p2Target');
+    let p3Target = $('#p3Target');
+    let p4Target = $('#p4Target');
+    let devBtn = $('#devBtn');
+    let p1Input = $('#p1-name input');
+    let p2Input = $('#p2-name input');
+    let p3Input = $('#p3-name input');
+    let p4Input = $('#p4-name input');
+    let startOutput = [];
 
-//////////////////////Start Page-load Modal//////////////////////
-let startModal = $('#start-modal');
-let bodyWrap = $('#body-wrap');
-let pSelect = document.querySelectorAll('.p-select-btn');
-let pSelected = 3;
-let p2 = $('#p2-name');
-let p3 = $('#p3-name');
-let p4 = $('#p4-name');
-let modalClose = $('#modalClose');
-let p1Target = $('#p1Target');
-let p2Target = $('#p2Target');
-let p3Target = $('#p3Target');
-let p4Target = $('#p4Target');
-let devBtn = $('#devBtn');
-let p1Input = $('#p1-name input');
-let p2Input = $('#p2-name input');
-let p3Input = $('#p3-name input');
-let p4Input = $('#p4-name input');
-let startOutput = [];
-
-const removeBorders = () => {
-    pSelect.forEach((e, i) => {
-        if (e.classList.contains('pSelected')) {
-            e.classList.remove('pSelected');
-        }
-    });
-};
-
-pSelect.forEach((e, i) => {
-    //handle clicking on a player select box
-    e.onclick = () => {
-        removeBorders();
-        e.classList.toggle('pSelected');
-        pSelected = (i);
-        if (pSelected === 0) {
-            p2.style.display = 'none';
-            p3.style.display = 'none';
-            p4.style.display = 'none';
-        } else if (pSelected === 1) {
-            p2.style.display = 'flex';
-
-            p3.style.display = 'none';
-            p4.style.display = 'none';
-        } else if (pSelected === 2) {
-            p2.style.display = 'flex';
-            p3.style.display = 'flex';
-
-            p4.style.display = 'none';
-        } else if (pSelected === 3) {
-            p2.style.display = 'flex';
-            p3.style.display = 'flex';
-            p4.style.display = 'flex';
-        }
-
+    const removeBorders = () => {
+        pSelect.forEach((e, i) => {
+            if (e.classList.contains('pSelected')) {
+                e.classList.remove('pSelected');
+            }
+        });
     };
-});
+
+    pSelect.forEach((e, i) => {
+        //handle clicking on a player select box
+        e.onclick = () => {
+            removeBorders();
+            e.classList.toggle('pSelected');
+            pSelected = (i);
+            if (pSelected === 0) {
+                p2.style.display = 'none';
+                p3.style.display = 'none';
+                p4.style.display = 'none';
+            } else if (pSelected === 1) {
+                p2.style.display = 'flex';
+
+                p3.style.display = 'none';
+                p4.style.display = 'none';
+            } else if (pSelected === 2) {
+                p2.style.display = 'flex';
+                p3.style.display = 'flex';
+
+                p4.style.display = 'none';
+            } else if (pSelected === 3) {
+                p2.style.display = 'flex';
+                p3.style.display = 'flex';
+                p4.style.display = 'flex';
+            }
+
+        };
+    });
 
 
 //page load popup modal
 //this is pretty sloppy so lmk if you need explaination
-const handleStartModal = () => {
-    let allGood = true;
-    const checkP4 = () => {
-        if (p4Input.value) {
-            p4Target.innerHTML = p4Input.value;
-        } else  {
-            allGood = false;
-            p3Target.innerHTML = '';
-            p4Input.style.border = 'red 2px solid';
-        }
-    };
-    const checkP3 = () => {
-        if (p3Input.value) {
-            p3Target.innerHTML = p3Input.value;
-        } else {
-            allGood = false;
-            p3Target.innerHTML = '';
-            p3Input.style.border = 'red 2px solid';
-        }
-    };
-    const checkP2 = () => {
-        if (p2Input.value) {
-            p2Target.innerHTML = p2Input.value;
-        } else {
-            allGood = false;
-            p2Input.style.border = 'red 2px solid';
-        }
-    };
-    const checkP1 = () => {
-        if (p1Input.value) {
-            p1Target.innerHTML = p1Input.value;
-        } else {
-            allGood = false;
-            p1Input.style.border = 'red 2px solid';
-        }
-    };
+    const handleStartModal = () => {
+        let allGood = true;
+        const checkP4 = () => {
+            if (p4Input.value) {
+                p4Target.innerHTML = p4Input.value;
+            } else  {
+                allGood = false;
+                p3Target.innerHTML = '';
+                p4Input.style.border = 'red 2px solid';
+            }
+        };
+        const checkP3 = () => {
+            if (p3Input.value) {
+                p3Target.innerHTML = p3Input.value;
+            } else {
+                allGood = false;
+                p3Target.innerHTML = '';
+                p3Input.style.border = 'red 2px solid';
+            }
+        };
+        const checkP2 = () => {
+            if (p2Input.value) {
+                p2Target.innerHTML = p2Input.value;
+            } else {
+                allGood = false;
+                p2Input.style.border = 'red 2px solid';
+            }
+        };
+        const checkP1 = () => {
+            if (p1Input.value) {
+                p1Target.innerHTML = p1Input.value;
+            } else {
+                allGood = false;
+                p1Input.style.border = 'red 2px solid';
+            }
+        };
 
-    if (pSelected === 3) {
-        checkP1();
-        checkP2();
-        checkP3();
-        checkP4();
-        startOutput.push(pSelected, p1Target.innerText, p2Target.innerText, p3Target.innerText, p4Target.innerText);
-    } else if (pSelected === 2) {
-        checkP1();
-        checkP2();
-        checkP3();
+        if (pSelected === 3) {
+            checkP1();
+            checkP2();
+            checkP3();
+            checkP4();
+            startOutput.push(pSelected, p1Target.innerText, p2Target.innerText, p3Target.innerText, p4Target.innerText);
+        } else if (pSelected === 2) {
+            checkP1();
+            checkP2();
+            checkP3();
+            if (allGood) {
+                $('#p4-box').style.display = 'none';
+                startOutput.push(pSelected, p1Target.innerText, p2Target.innerText, p3Target.innerText);
+            }
+        } else if (pSelected === 1) {
+            checkP1();
+            checkP2();
+            if (allGood) {
+                $('#p3-box').style.display = 'none';
+                $('#p4-box').style.display = 'none';
+                startOutput.push(pSelected, p1Target.innerText, p2Target.innerText);
+            }
+        } else if (pSelected === 0) {
+            allGood = false;
+            $('#lonely').style.display = 'flex';
+        }
+
         if (allGood) {
-            $('#p4-box').style.display = 'none';
-            startOutput.push(pSelected, p1Target.innerText, p2Target.innerText, p3Target.innerText);
+            startModal.style.display = "none";
+            bodyWrap.style.display = "flex";
+            console.log('OUTPUT: ' , startOutput);
         }
-    } else if (pSelected === 1) {
-        checkP1();
-        checkP2();
-        if (allGood) {
-            $('#p3-box').style.display = 'none';
-            $('#p4-box').style.display = 'none';
-            startOutput.push(pSelected, p1Target.innerText, p2Target.innerText);
-        }
-    } else if (pSelected === 0) {
-        allGood = false;
-        $('#lonely').style.display = 'flex';
-    }
+    };
 
-    if (allGood) {
-        startModal.style.display = "none";
-        bodyWrap.style.display = "flex";
-        console.log('OUTPUT: ' , startOutput);
-    }
-};
-
-modalClose.onclick = () => {
-    handleStartModal();
-};
+    modalClose.onclick = () => {
+        handleStartModal();
+    };
 
 //remove this for product
-devBtn.onclick = () => {
-    p1Input.value = 'Juan';
-    p2Input.value = 'Dicky';
-    p3Input.value = 'Jonny';
-    p4Input.value = 'Hunter';
-};
+    devBtn.onclick = () => {
+        p1Input.value = 'Juan';
+        p2Input.value = 'Dicky';
+        p3Input.value = 'Jonny';
+        p4Input.value = 'Hunter';
+    };
 
 
-$('#p-num-default').classList.add('pSelected');
+    $('#p-num-default').classList.add('pSelected');
 
 //////////////////////End Page-load Modal//////////////////////
 
 //////////////////////Start Property Popup Modal//////////////////////
 
 //property popup modal
-const showProperty = (ele) => {
-    let propModal = $('#property-modal');
-    let propModalContent = $('#prop-modal-content');
-    let cardName = ele.split('-')[1];
-    let close = $('#close-prop');
+    const showProperty = (ele) => {
+        let propModal = $('#property-modal');
+        let propModalContent = $('#prop-modal-content');
+        let cardName = ele.split('-')[1];
+        let close = $('#close-prop');
 
-    //show modal
-    propModal.style.display = "flex";
-    propModalContent.style.backgroundImage = `url("./assets/Cards/${cardName}.jpg")`;
+        //show modal
+        propModal.style.display = "flex";
+        propModalContent.style.backgroundImage = `url("./assets/Cards/${cardName}.jpg")`;
 
-    //Close if click outside
-    window.onclick = e => {
-        if (e.target === propModal) {
-            propModal.style.display = "none";
-        }
-        if (e.target === close) {
-            propModal.style.display = "none";
-        }
+        //Close if click outside
+        window.onclick = e => {
+            if (e.target === propModal) {
+                propModal.style.display = "none";
+            }
+            if (e.target === close) {
+                propModal.style.display = "none";
+            }
+        };
     };
-};
 
 //////////////////////End Property Popup Modal//////////////////////
 
 //bind the property tiles
-document.querySelectorAll('.grid').forEach((e, i) => {
-    e.onclick = () => {
-        showProperty(e.id);
-    };
-});
+    document.querySelectorAll('.grid').forEach((e, i) => {
+        e.onclick = () => {
+            showProperty(e.id);
+        };
+    });
 
-export {$, showProperty}
+};
+
+
+
+export {$, dom}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { startGame } from './game.js';
+import {dom} from './dom.js';
+dom();
 
 //TODO: on PLAY button click make players
 //query for Play Buttons
